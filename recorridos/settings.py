9dac8211b060cdcd7a459c1873b6e7e387fe8f2a
root <root@debian-s-1vcpu-1gb-tor1-01.localdomain>
2019-09-18 18:54:31 +0000

    change settings

--- recorridos/settings.py
+++ recorridos/settings.py
@@ -81,11 +81,21 @@ WSGI_APPLICATION = 'recorridos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'recorridos',
+            'USER': 'user_recorridos',
+            'PASSWORD': 'PrimeroRiver',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
 }
 
 
